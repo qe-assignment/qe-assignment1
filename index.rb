50e2238c1ec6b75ae79405975925b941aa2ea940
Sverker Rasmuson <qe.assignment1@gmail.com>
2015-04-18 16:31:08 +0200

    Added twitter api and print out tweets according to topic

--- index.rb
+++ index.rb
@@ -1,5 +1,28 @@
 require 'sinatra'
+require 'twitter'
+require 'json'
 
+def setup_twitter_client
+    file = File.read('twitter-keys.json')
+    keys = JSON.load(file)
+    client = Twitter::Streaming::Client.new do |config|
+        config.consumer_key        = keys["consumer_key"]
+        config.consumer_secret     = keys["consumer_secret"]
+        config.access_token        = keys["access_token"]
+        config.access_token_secret = keys["access_token_secret"]
+    end
+
+    return client
+end
+
+twitter_client = setup_twitter_client
+
+topics = ["fashion"]
 get '/' do 
-    "test"
+    stream do |out|
+            twitter_client.filter(track: topics.join(",")) do |object|
+                tweet = object if object.is_a?(Twitter::Tweet) 
+                out << tweet.text
+            end
+    end
 end
